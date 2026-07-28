b710e715a7c9a02ab2c1961b68b63d275723facf
Sagar Naik <sgar3286@gmail.com>
2026-07-28 23:30:25 +0530

    Time: 2 ms (99.07%), Space: 46.8 MB (12.29%) - LeetHub

--- 2535-difference-between-element-sum-and-digit-sum-of-an-array/2535-difference-between-element-sum-and-digit-sum-of-an-array.java
+++ 2535-difference-between-element-sum-and-digit-sum-of-an-array/2535-difference-between-element-sum-and-digit-sum-of-an-array.java
@@ -1,20 +1,35 @@
+// class Solution {
+//     public int differenceOfSum(int[] nums) {
+//        int Esum = 0;
+//        int sum = 0;
+       
+//        for(int i = 0;i<nums.length;i++){
+//         sum = sum + nums[i];
+//        }
+//        for(int i = 0;i<nums.length;i++){
+//         int val = nums[i];
+//             while(val!=0){
+//             Esum = Esum + val%10;
+//             val = val/10;
+//             }
+//        }
+//        int sub = Math.abs(sum - Esum);
+//        return sub;
+//     }
+// }
+
 class Solution {
     public int differenceOfSum(int[] nums) {
-       int Esum = 0;
-       int sum = 0;
-       
-       for(int i = 0;i<nums.length;i++){
-        sum = sum + nums[i];
-       }
-       for(int i = 0;i<nums.length;i++){
-        int val = nums[i];
-            while(val!=0){
-            Esum = Esum + val%10;
-            val = val/10;
+        int Esum = 0;
+        int sum = 0;
+        for(int num : nums){
+            sum = sum + num;
+            int temp = num;
+            while(temp > 0){
+                Esum = Esum + temp%10;
+                temp = temp/10;
             }
-       }
-       int sub = Math.abs(sum - Esum);
-       return sub;
+        }
+        return Math.abs(Esum - sum);
     }
 }
-
