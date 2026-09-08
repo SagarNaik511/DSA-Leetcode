dcfabcb6fe4962960074859cbddf2ca6132992d5
Sagar Naik <sgar3286@gmail.com>
2026-09-08 21:14:33 +0530

    Time: 3 ms (92.81%), Space: 48.8 MB (19.44%) - LeetHub

--- 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.java
+++ 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.java
@@ -1,30 +1,60 @@
+// class Solution {
+//     public int maxSatisfied(int[] customers, int[] grumpy, int minutes) {
+//       int num = 0;
+//       int[] arr = new int[customers.length];
+//       for(int i=0;i<customers.length;i++){
+//         if(grumpy[i] == 0){
+//             num = num + customers[i];
+//         }
+//       }
+//        for(int i=0;i<customers.length;i++){
+//         if(grumpy[i] == 1){
+//             arr[i] = customers[i];
+//         }
+//       }
+
+//      int window = 0;
+//      int maxCus = 0;
+//      int left = 0;
+
+//      for(int right = 0; right<arr.length; right++){
+//       window = window + arr[right];
+//       if(right - left + 1 == minutes){
+//         maxCus = Math.max(maxCus, window);
+//         window = window - arr[left];
+//         left++;
+//       }
+//      } 
+//      return maxCus + num;
+//     }
+// }
+
 class Solution {
     public int maxSatisfied(int[] customers, int[] grumpy, int minutes) {
-      int num = 0;
-      int[] arr = new int[customers.length];
-      for(int i=0;i<customers.length;i++){
-        if(grumpy[i] == 0){
-            num = num + customers[i];
+        int n = customers.length;
+        int happy = 0;
+        for(int i=0;i<n;i++){
+            if(grumpy[i] == 0){
+                happy = happy + customers[i];
+            }
         }
-      }
-       for(int i=0;i<customers.length;i++){
-        if(grumpy[i] == 1){
-            arr[i] = customers[i];
+        int window = 0;
+        for(int i=0;i<minutes;i++){
+            if(grumpy[i] == 1){
+                window = window + customers[i];
+            }
         }
-      }
-
-     int window = 0;
-     int maxCus = 0;
-     int left = 0;
-
-     for(int right = 0; right<arr.length; right++){
-      window = window + arr[right];
-      if(right - left + 1 == minutes){
-        maxCus = Math.max(maxCus, window);
-        window = window - arr[left];
-        left++;
-      }
-     } 
-     return maxCus + num;
+        int max = window;
+        for(int right = minutes; right < n; right++){
+            if(grumpy[right]==1){
+                window = window + customers[right];
+            }
+            int left = right - minutes;
+            if(grumpy[left]==1){
+                window = window - customers[left];
+            }
+            max = Math.max(max, window);
+        }
+        return max + happy;
     }
 }
